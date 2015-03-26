ff3af0f80316e8645a6ed34e5bbabb1e34fd4d69
Darian C. Springer <dcss@itserver6.cs.umb.edu>
2015-03-26 18:43:07 -0400

    Added quit functionality

--- hw2.cpp
+++ hw2.cpp
@@ -4,13 +4,22 @@ using namespace std;
 
 void printMenu()
 {
-	cout << "\n===== STUDENT DATABASE MANAGEMENT SYSTEM =====\n\n\n";
-	cout << "\t1. Add Records\n";
-	cout << "\t2. List Records\n";
-	cout << "\t3. Modify Records\n";
-	cout << "\t4. Delete Records\n";
-	cout << "\t5. Exit Program\n\n";
-	cout << "Select Your Choice :=> ";
+	int choice = 0;
+
+	while (choice != 5)
+	{
+		cout << "\n===== STUDENT DATABASE MANAGEMENT SYSTEM =====\n\n\n";
+		cout << "\t1. Add Records\n";
+		cout << "\t2. List Records\n";
+		cout << "\t3. Modify Records\n";
+		cout << "\t4. Delete Records\n";
+		cout << "\t5. Exit Program\n\n";
+		cout << "Select Your Choice :=> ";
+
+		cin >> choice;
+		cout << choice << "\n\n";
+	}
+	cout << "\n\n\tThank you very much for using the Student Database Management System\n\n";
 
 }
 
