cdff028c0c486b70b10ec7bc0715c969fc41c89b
Darian C. Springer <dcss@itserver6.cs.umb.edu>
2015-03-26 18:30:39 -0400

    Initial commit

---
+++ hw2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+
+void printMenu()
+{
+	cout << "\n===== STUDENT DATABASE MANAGEMENT SYSTEM =====\n\n\n";
+	cout << "\t1. Add Records\n";
+	cout << "\t2. List Records\n";
+	cout << "\t3. Modify Records\n";
+	cout << "\t4. Delete Records\n";
+	cout << "\t5. Exit Program\n\n";
+	cout << "Select Your Choice :=> ";
+
+}
+
+int main()
+{
+	//int i;
+	printMenu();
+	return 0;
+}
