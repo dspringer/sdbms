7c8f1b877b04dee9dd289f0cc8c1cacef8e7a559
Darian C. Springer <dcss@itserver6.cs.umb.edu>
2015-03-26 21:33:07 -0400

    Added student class

--- hw2.cpp
+++ hw2.cpp
@@ -1,6 +1,65 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+class student
+{
+
+	private:
+	// Student Full Name
+	char* name;
+
+	// Student id, cannot be null and will be auto-incremented
+	int student_id;
+	// Is used for the auto-incrementation of the the student id
+	static int next_id;
+
+	// Indicates if a student is a Freshman, Sophomore, Junior, Senior
+	// Freshman == 1
+	// Sophomore == 2
+	// Junior == 3
+	// Senior == 4
+	int year_of_study;
+
+	// Indicated if a student is Female(true) or a Male(false)
+	bool gender;
+
+
+	public:
+	static int next_user_id()
+	{
+		next_id++;
+		return next_id;
+	}
+
+	void setName(char* x)
+	{
+		while(*x){
+			*student::name = *x;
+			x++;
+			student::name++;
+		}
+		*student::name = '\0';
+	}
+
+	void setGender (bool x)
+	{
+		gender = x;
+	}
+
+	void setYear(int x)
+	{
+		if( x > 0  && x < 5)
+		 	year_of_study = x;
+	}
+
+	student()
+	{
+		student_id = student::next_id++;
+	}
+};
+
+int student::next_id = 0;
 
 void printMenu()
 {
